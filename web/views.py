b2b2d6c6fa5aa32f6044378a7677d85108668294
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2021-10-14 01:16:25 -0300

    creating cadastrar_diarista route

--- web/views.py
+++ web/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import diarista_form
+
 
 # Create your views here.
+
+def cadastrar_diarista(request):
+    form_diarista = diarista_form.DiaristaForm()
+    return render(request, 'form_diarista.html', {'form-diarista': form_diarista})
